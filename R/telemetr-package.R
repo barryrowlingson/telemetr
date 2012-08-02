7483b1063b08a897465fe9bc263d9585dec83899
Barry Rowlingson <b.rowlingson@gmail.com>
2012-08-02 23:35:21 +0100

    new data

---
+++ R/telemetr-package.R
@@ -0,0 +1,22 @@
+#' Location finding by direction telemetry
+#'
+#' Using bearings and locations to find things.
+#'
+#' @name telemetr-pacakge
+#' @aliases telemetr
+#' @docType package
+#' @title Location finding by directional telemetry
+#' @author \email{B.Rowlingson@@gmail.com}
+#' @keywords package
+NULL
+#' sample azimuth data from radiotrk
+#'
+#' bearings and locations supplied with the original radiotrk zip file
+#'
+#' @name azr
+#' @docType data
+#' @format a spatial points data frame
+#' @keywords datasets
+NULL
+
+
